34cfcf8687153da9f561d41c5943d705bdf6f43a
Azziiieee <balendez.alodiazenny26@gmail.com>
2025-04-03 22:35:23 +0800

    RECURSIVE METHOD FIBONACCI

---
+++ Balendez_IT13A/src/Midterm/Lab_Act6_Recursive_Method_Fibonacci.java
@@ -0,0 +1,21 @@
+
+package Midterm;
+
+
+public class Lab_Act6_Recursive_Method_Fibonacci {
+    public static void main(String[] args) {
+        int x = 10;
+        
+        for (int i = 0; i < x; i++) {
+            System.out.print(fibonacci(i) + ", ");
+        }
+    }
+    public static int fibonacci(int x) {
+        if (x <= 1) {
+            return x;
+        } else {
+            return fibonacci(x - 1) + fibonacci(x - 2);
+        }
+    }
+    
+}
